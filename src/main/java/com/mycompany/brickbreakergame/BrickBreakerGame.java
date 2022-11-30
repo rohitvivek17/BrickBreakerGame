0a84b9d01a7ede6773687deb24b24c323ae0d7a7
rohitvivek17 <88471520+rohitvivek17@users.noreply.github.com>
2022-11-30 22:46:54 +0530

    first commit

---
+++ src/main/java/com/mycompany/brickbreakergame/BrickBreakerGame.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.brickbreakergame;
+
+import javax.swing.JFrame;
+
+/**@author VIVEK */
+public class BrickBreakerGame {
+
+    public static void main(String[] args) {
+       JFrame obj=new JFrame();
+        GamePlay gamePlay=new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Brick Breaker Game");
+        obj.setTitle("Brick Breaker Game"+"   "+"[Use Left & Right Arrow Keys]");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
